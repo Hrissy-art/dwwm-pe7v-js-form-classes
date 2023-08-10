39b296af7768121c37c26060a771c7b55bd855b5
Hrissy-art <hristinastankova89@gmail.com>
2023-08-10 14:38:16 +0200

    main

---
+++ _js/misc.js
@@ -0,0 +1,15 @@
+"use strict;"
+
+function sayMessage(msg)
+{
+    console.log(msg);
+}
+
+function sayHello()
+{
+    sayMessage("Début du programme 'Nombre mystère'");
+}
+function sayGoodbye()
+{
+    sayMessage("Fin du programme 'Nombre mystère'");
+}
