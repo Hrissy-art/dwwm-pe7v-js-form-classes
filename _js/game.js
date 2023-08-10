9cbad3667d403b80b3465d5f58a560dbed254948
Hrissy-art <hristinastankova89@gmail.com>
2023-08-10 14:44:28 +0200

    main

--- _js/game.js
+++ _js/game.js
@@ -1,98 +1,22 @@
-"use strict;"
+"use strict";
 
-const MIN_RANDOM_NUMBER = 1;
-const MAX_RANDOM_NUMBER = 10;
+class Game {
+    secret;
+    turn;
+    start(playerName) {
+        console.log(`Bienvenue dans le jeu du nombre mystère, ${playerName}!`);
+        inputList = [];
+        this.secret = getRandomNumber();
+        this.turn = 0;
+        console.log("nombre mystère : " + this.secret);
 
-// Liste des propositions
-var inputList = [];
+        let hasFoundNumber = false;
 
-function askPlayerName()
-{
-    return prompt("Quel est votre nom de joueur svp ?");
-}
+        while (!hasFoundNumber) {
+            this.turn++;
+            hasFoundNumber = makePlayerGuess(this.secret);
+        }
 
-function getRandomNumber()
-{
-    return (Math.floor(Math.random() * MAX_RANDOM_NUMBER)+1);
-}
-
-function askPlayerInput()
-{
-    let inputMsg = "";
-    if (inputList.length > 0){
-        inputMsg = "\nVous avez déjà proposé " + inputList.join(",");
+        sayMessage(`Fin de la partie en ${this.turn} essais...`);
     }
-    return prompt(`Quelle est votre proposition entre ${MIN_RANDOM_NUMBER} et ${MAX_RANDOM_NUMBER} ?${inputMsg}`);
 }
-
-function checkPlayerInput(playerInput, numberToGuess)
-{
-    if (playerInput > MAX_RANDOM_NUMBER) {
-        alert(`On a dit un nombre inférieur ou égal à ${MAX_RANDOM_NUMBER} !!`);
-        return false;
-    }
-    if (playerInput < MIN_RANDOM_NUMBER) {
-        alert(`On a dit un nombre supérieur ou égal à ${MIN_RANDOM_NUMBER} !!`);
-        return false;
-    }
-    if (playerInput > numberToGuess) {
-        sayMessage(`Votre proposition est plus grande que le nombre mystère !`);
-        return false;
-    }
-    if (playerInput < numberToGuess) {
-        sayMessage(`Votre proposition est plus petite que le nombre mystère !`);
-        return false;
-    }
-    if (playerInput == numberToGuess) {
-        sayMessage(`Félicitations,le nombre mystère est bien ${numberToGuess} !`);
-        return true;
-    }
-    return false;
-}
-
-function makePlayerGuess(numberToGuess)
-{
-    let playerInput = askPlayerInput();
-    console.log("le joueur a saisi : " + playerInput);
-    inputList.push(playerInput);
-    return checkPlayerInput(playerInput, numberToGuess);
-}
-
-function runGame(playerName)
-{
-    console.log(`Bienvenue dans le jeu du nombre mystère, ${playerName}!`);
-    inputList = [];
-    let numberToGuess = getRandomNumber();
-    console.log("nombre mystère : " + numberToGuess);
-
-    let hasFoundNumber = false;
-
-    while (!hasFoundNumber) {
-        hasFoundNumber = makePlayerGuess(numberToGuess);
-    }
-
-    sayMessage("Fin de la partie...");
-
-}
-
-function askPlayerToPlayAgain()
-{
-    return confirm("Voulez-vous faire une nouvelle partie ?");
-}
-
-function main()
-{
-    sayHello();
-
-    let playerName = askPlayerName();
-    
-    let startAGame = true;
-    while (startAGame) {
-        runGame(playerName);
-        startAGame = askPlayerToPlayAgain();
-    }
-    
-    sayGoodbye();
-}
-
-main();
